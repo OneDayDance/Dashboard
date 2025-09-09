d51188189c1b9865a953f73789723cf2bda674b4
OneDayDance <joseph@onedaydance.com>
2025-09-08 21:32:11 -0400

    Config Hotfix

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 // js/config.js
 // Description: Holds the static configuration variables for the application.
 
-export const CLIENT_ID = "555797317893-ce2nrrf4e5dol0c6lurln0c3it76c2r.apps.googleusercontent.com";
+export const CLIENT_ID = "555797317893-ce2nrrf49e5dol0c6lurln0c3it76c2r.apps.googleusercontent.com";
 export const SPREADSHEET_ID = "1G3kVQdR0yd1j362oZKYRXMby1Ve6PVcY8CrsQnuxVfY";
 export const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+
